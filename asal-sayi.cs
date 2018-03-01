18693a0f288636553b01a048da6f0d02ccaf1cd4
İbrahim YAVUZ <ibrahimyavuz92@outlook.com>
2018-03-01 21:33:44 +0300

    C#'ta 1-999 Arasındaki Asal Sayıları Bulan Program

---
+++ asal-sayi.cs
@@ -0,0 +1,14 @@
+static void Main(string[] args)
+{
+    Console.WriteLine("\n-------- 1-999 Arasındaki Asal Sayılar --------\n");
+    for (int i = 1; i <= 999; i++)
+    {
+        int syc = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % j == 0) syc++;
+        }
+        if (syc == 2) Console.Write("\t" + i);
+    }
+    Console.ReadLine();
+}
