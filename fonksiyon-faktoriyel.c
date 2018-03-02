b244bf0fd17fd452a26e97280d968e05afdb04df
İbrahim YAVUZ <ibrahimyavuz92@outlook.com>
2018-03-02 23:30:32 +0300

    C'de Fonksiyon Faktöriyel

---
+++ fonksiyon-faktoriyel.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int fakt(int n)
+{
+	int i, f  = 1;
+	for (i = n; i >= 1; i--)
+	{
+		f *= i;
+	}
+	return f;
+}
+
+int main(void)
+{
+	int s;
+	printf("Enter an integer number: ");
+	scanf("%d", &s);
+	printf("The factorial of %d is %d.\n\n", s, fakt(s));
+	system("pause");
+	return 0;
+}
