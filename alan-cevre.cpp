f5786197c1dfd087edf18ecb703afafe14e28ab5
İbrahim YAVUZ <ibrahimyavuz92@outlook.com>
2018-03-02 23:09:18 +0300

    C++'ta Alan ve Çevre Hesaplama

---
+++ alan-cevre.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+#define PI 3.14
+using namespace std;
+
+int main(void)
+
+{
+	double yaricap, alan, cevre;
+	cout << "Yaricapi Giriniz: ";
+	cin >> yaricap;
+	alan = PI * yaricap * yaricap;
+	cevre = 2 * PI * yaricap;
+	cout << "Cevresi: " << cevre << endl << "Alan: " << alan << endl << endl;
+	system("PAUSE");
+	return 0;
+}
