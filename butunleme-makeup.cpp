a57fb6da1117f281c8e36cb6b978877a54cfc642
İbrahim YAVUZ <ibrahimyavuz92@outlook.com>
2018-03-02 23:20:36 +0300

    C++'ta Bütünleme (Makeup)

---
+++ butunleme-makeup.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int grade, makeup;
+	cout << "enter grade: ";
+	cin >> grade;
+	if (grade >= 50) cout << "PASS" << endl << endl;
+	else
+	{
+		cout << "enter make up grade: ";
+		cin >> makeup;
+		if (makeup >= 60) cout << "Success!" << endl << endl;
+		else cout << "Fail!" << endl << endl;
+	}
+	system("PAUSE");
+	return 0;
+}
