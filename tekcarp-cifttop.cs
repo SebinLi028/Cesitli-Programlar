e40bf1fda3a95b854a3471de83480e72d7708a72
İbrahim YAVUZ <ibrahimyavuz92@outlook.com>
2018-03-04 15:31:24 +0300

    C#'ta Tek Sayılar Çarpım ve Çift Sayılar Toplam

---
+++ tekcarp-cifttop.cs
@@ -0,0 +1,16 @@
+private void button1_Click(object sender, EventArgs e)
+{
+	int sayi = int.Parse(textBox1.Text), tekc = 1, ciftt = 0;
+	for (int i = 1; i <= sayi; i++)
+	{
+		if (i % 2 == 1)
+		{
+			tekc *= i;
+		}
+		if (i % 2 == 0)
+		{
+			ciftt += i;
+		}
+	}
+	MessageBox.Show("Tek Çarpım: " + tekc.ToString() + "\n" + "Çift Toplam: " + ciftt.ToString());
+}
