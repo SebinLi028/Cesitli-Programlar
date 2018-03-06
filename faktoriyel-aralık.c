1fd7c49daf295a517630dd407f7bd40ebefd7de1
İbrahim YAVUZ <ibrahimyavuz92@outlook.com>
2018-03-06 12:51:50 +0300

    C'de 1'den Girilen Sayıya Kadar Faktöriyel

--- faktoriyel-aralık.c
+++ faktoriyel-aralık.c
@@ -6,17 +6,9 @@ int main()
 	int sayi, fakt = 1, i;
 	printf("Sayi Giriniz: ");
 	scanf("%d", &sayi);
-	if (sayi == 0) 
-	{
-		printf("0!= 1\n");
-	}
+	if (sayi == 0) printf("0!= 1\n");
 	else if (sayi > 0)
-	{
-		for (i = 1; i <= sayi; i++)
-		{
-			printf("%d!= %d\n", i, fakt *= i);
-		}
-	}
+	{ for (i = 1; i <= sayi; i++) printf("%d!= %d\n", i, fakt *= i); }
 	system("pause");
 	return 0;
 }
