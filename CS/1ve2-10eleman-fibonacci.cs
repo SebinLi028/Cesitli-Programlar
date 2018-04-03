b9a987bfb4bd3d30f3cf197f411609505d9c3aa5
İbrahim YAVUZ <ibrahimyavuz92@outlook.com>
2018-04-03 09:41:27 +0300

    1 ve 2 İle Başlayan 10 Elemanlı Çift Fibonacci Serisi

--- CS/1ve2-10eleman-fibonacci.cs
+++ CS/1ve2-10eleman-fibonacci.cs
@@ -1,19 +1,14 @@
-static int sayi1 = 1, sayi2 = 1, gecici = 0;
-static int fibo()
-{
-    Console.Write(sayi1 + "\t");
-    gecici = sayi1;
-    sayi1 = sayi1 + sayi2;
-    sayi2 = gecici;
-    return gecici;
-}
-
 static void Main(string[] args)
 {
-    int[] dizi = new int[10];//fibonacci terim sayısı: 10
-    int toplam = 0;
-    for (int i = 0; i < dizi.Length; i++) dizi[i] = fibo();//fibo elemanlarını diziya aktar
-    for (int i = 0; i < dizi.Length; i++) if (dizi[i] % 2 == 0) toplam += dizi[i];//dizideki elemanların çift olanlarını topla
-    Console.WriteLine("\n\nToplam Çift Fibonacci: " + toplam);
+    int sayi1 = 1, sayi2 = 1, gecici = 0, toplam = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write("{0}\t", sayi1);
+        gecici = sayi1;
+        sayi1 = sayi1 + sayi2;
+        sayi2 = gecici;
+        if (gecici % 2 == 0) toplam += gecici;
+    }
+    Console.WriteLine("\n\n Toplam Çift Fibonacci: {0}", toplam);
     Console.ReadKey();
 }
